13cfd5903feb086fef793b1f8f8c7a897fa68457
yasmin <yasminmohamudyasmin@gmail.com>
2025-11-28 22:05:39 +0300

    add a login tracker function to index.js

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
+function createLoginTracker(userInfo) {
+  
+  let attemptCount = 0;
 
+  
+  return (passwordAttempt) => {
+    attemptCount++; 
+
+    
+    if (attemptCount > 3) {
+      return "Account locked due to too many failed login attempts";
+    }
+
+    if (passwordAttempt === userInfo.password) {
+      return "Login successful";
+    } else {
+      return `Attempt ${attemptCount}: Login failed`;
+    }
+  };
+}
 
 
 module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
+  ...(typeof createLoginTracker !== "undefined" && { createLoginTracker }),
 };
